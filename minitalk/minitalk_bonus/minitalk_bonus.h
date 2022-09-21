7c96455221de5be9a6e9450f38b9ac88c9a3424f
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-09-21 22:11:33 +0900

    update

--- minitalk/minitalk_bonus/minitalk_bonus.h
+++ minitalk/minitalk_bonus/minitalk_bonus.h
@@ -6,15 +6,19 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 22:15:33 by myoshika          #+#    #+#             */
-/*   Updated: 2022/09/12 00:03:27 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/09/16 23:10:25 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
+# include <signal.h>
 # include <unistd.h>
 # include <sys/types.h>
+# include <stdarg.h>
+# include <stdlib.h>
 # include "../lib/libft/libft.h"
+# include "../lib/printf/ft_printf.h"
 
 #endif
--- minitalk/minitalk_mandatory/server.c
+++ minitalk/minitalk_mandatory/server.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 22:14:08 by myoshika          #+#    #+#             */
-/*   Updated: 2022/09/18 05:10:30 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/09/21 22:10:55 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ static void	handler(int signal, siginfo_t s_info, void *context)
 	static int	bit;
 	pid_t		client_pid;
 
+	(void)context;
 	client_pid = s_info->si_pid;
 	if (signal == SIGUSR1)
 		c = c << 1;
@@ -25,8 +26,6 @@ static void	handler(int signal, siginfo_t s_info, void *context)
 		c = (c << 1) + 1;
 	if (++bit == 8)
 	{
-		if (c == 4)
-			kill(client_pid, SIGUSR2);
 		ft_putchar_fd(c, 1);
 		bit = 0;
 		c = 0;
@@ -35,29 +34,15 @@ static void	handler(int signal, siginfo_t s_info, void *context)
 
 int	main(void)
 {
-	struct sigaction	s;
+	struct sigaction	sa;
 
-	s.sa_flags = SA_SIGINFO;
-	s.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
 	ft_printf("pid: %d\n", getpid());
-	if (sigaction(SIGUSR1, &handler, NULL) == -1)
+	if (sigaction(SIGUSR1, &handler, NULL) != 0)
 		exit (1);
-	if (sigaction(SIGUSR2, &handler, NULL) == -1)
+	if (sigaction(SIGUSR2, &handler, NULL) != 0)
 		exit (1);
 	while (1)
 		pause ;
 }
-
-/*
-do i really need to use pause?
-make sure to check that acknowledge actually works
-
-かわださんのに追記で、UTF-8では上位ビットを見ることでそのバイトがどの文字に属するのかがわかるようになっています。1文字の1byte目は、 0 , 110 , 1110 , 11110 のいずれかが上位ビットにきます。それぞれ、1byte, 2byte, 3byte, 4byteで1文字を表すことを示します。2~4byte文字の場合、それ以降に 10 で続くバイト列が並びます。
-まとめると、
-- 1byte文字 → 0XXXXXXX 
-- 2byte文字 → 110XXXXX 10XXXXXX
-- 3byte文字 → 1110XXXX 10XXXXXX 10XXXXXX 
-- 4byte文字 → 11110XXX 10XXXXXX 10XXXXXX 10XXXXXX 
-の形式を取ることになります。
-少なくとも1byteずつ読んでいってもASCIIであるか否かは最上位bitだけ確認すれば良いということになります。
-*/
